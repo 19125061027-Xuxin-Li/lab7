d6e90a643fc63b14848ed5386058e292f38b7c91
Xuxin Li <lxx@LAPTOP-H18K17RN>
2021-11-02 17:38:53 +0800

    Second submission

--- src/Lab7/SavingsAccount.java
+++ src/Lab7/SavingsAccount.java
@@ -2,5 +2,26 @@ package Lab7;
 
 public class SavingsAccount extends BankAccount {
 	
+	private double percentage;
+	private double fee;
+	
+	public SavingsAccount(String username,double cash, double fee, double percentage) {
+		super(username,cash);
+		this.fee = fee;
+		this.percentage = percentage;
+	}
+	
+	public void depositing(double money) {
+		if(money>0) {
+			balance=percentage*(balance+money);
+		}else {
+			System.out.println("The deposit amount cannot be less than or equal to 0!");
+		}
+	}
+	public void withdrawing(double money) {
+		if (money>0) {
+			balance=balance-money-percentage*money;
+		}
+	}
 
 }
